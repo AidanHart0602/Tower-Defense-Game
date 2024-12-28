99b36126a1b101d376f93328117965df0e2108f8
AidanHart0602 <aidanhart321@gmail.com>
2024-12-27 23:27:36 -1000

    Spawn Manager Pt.1 (AKA, Day 8)

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -5,40 +5,74 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
+    private UIManager _uiManager;
     private List<GameObject> _enemyPool;
     [SerializeField]
-    private GameObject _enemyType;
+    private GameObject[] _enemyType;
     [SerializeField]
     private GameObject _enemyContainer;
     [SerializeField]
     private GameObject _startingPoint;
-    private int _enemyLimit = 325;
-    [SerializeField]
-    private int _spawnedEnemies = 0;
-    [SerializeField]
-    private int _numberOfEnemies;
-    private bool _gameActive = true;
+
+    private int _waveNum, _waveEnemyNum, _spawnedEnemies;
+    private int _waveEnemyLimit = 5;
     List<GameObject> _storeEnemies(int NumOfEnems)
     {
-        for(int i = 0; i < NumOfEnems; i++) 
+        for (int i = 0; i < NumOfEnems; i++)
         {
-            GameObject enemy = Instantiate(_enemyType);
+            GameObject enemy = Instantiate(_enemyType[0]);
             enemy.transform.position = _startingPoint.transform.position;
+            enemy.SetActive(false);
             _enemyPool.Add(enemy);
         }
         return _enemyPool;
     }
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        _storeEnemies(10);
+        NewWave();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void NewWave() 
     {
-        
+        _waveNum++;
+        _uiManager.waveUpdate(_waveNum);
+        if (_waveNum == 11)
+        {
+            //Start the end sequence
+            return;
+        }
+        _waveEnemyLimit += 5;
+        _waveEnemyNum = _waveEnemyLimit;
+        _uiManager.enemyNumChange(_waveEnemyNum);
+        _spawnedEnemies = 0;
+        StartCoroutine(SpawnAI());
     }
 
-//    IEnumerator StoreAI
+    public void ReduceNumbers() 
+    {
+        _waveEnemyNum--;
+        _uiManager.enemyNumChange(_waveEnemyNum);
+    }
+    IEnumerator SpawnAI()
+    {
+        while((_spawnedEnemies > _waveEnemyLimit ) && (_spawnedEnemies != _waveEnemyLimit))
+        {
+            foreach(var enemy in _enemyPool) 
+            {
+                if(enemy.activeInHierarchy == false)
+                {
+                    _spawnedEnemies++;
+                    enemy.SetActive(true);
+                    break;
+                }
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        if((_spawnedEnemies == _waveEnemyLimit) && _waveNum != 11)
+        {
+            NewWave();
+        }
+    }
 }
--- UIManager.cs
+++ UIManager.cs
@@ -8,7 +8,8 @@ public class UIManager : MonoBehaviour
 {
     public int money;
     [SerializeField] private TMP_Text moneyText;
-
+    [SerializeField] private TMP_Text enemyNumText;
+    [SerializeField] private TMP_Text waveNumText;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,16 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         moneyText.text = "" + money;    
+    }
+
+    public void enemyNumChange(int numOfEnemies)
+    {
+        enemyNumText.text = "" + numOfEnemies;
+    }
+
+    public void waveUpdate(int waveNum)
+    {
+        waveNumText.text = "" + waveNum;
     }
 
     public void Restart()
